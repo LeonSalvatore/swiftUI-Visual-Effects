29e5b8804c3e4e70fb9be2f515fa364ac7737f80
Leon Salvatore <elione007@gmail.com>
2025-04-30 14:11:14 +0200

    Initial Commit

---
+++ swiftUI Visual Effects/swiftUI_Visual_EffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_Visual_EffectsApp.swift
+//  swiftUI Visual Effects
+//
+//  Created by Leon Salvatore on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_Visual_EffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
